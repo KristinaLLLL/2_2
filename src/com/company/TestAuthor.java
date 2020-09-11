7948e8ef11658bd2413ac56cf0bef31f49c4421f
KristinaLLLL <k.27.06.2001.l@gmail.com>
2020-09-11 17:10:10 +0300

    Initial commit

---
+++ src/com/company/TestAuthor.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1=new Author("Lopatina K.P.", "xxxx@xx.ru", 'w');
+        System.out.println(a1);
+    }
+}
